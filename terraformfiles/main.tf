86ad38c82a139adb477563490b913ca0a441ebe7
Ganesha2003 <115620909+Ganesha2003@users.noreply.github.com>
2025-11-22 09:10:15 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0fa3fe0fa7920f68e"
+  ami = "ami-0fa3fe0fa7920f68e"                            #replace with your AMI ID of amazon linux 
   instance_type = "t3.small"
   key_name = "bookmyshow"
-  vpc_security_group_ids = ["sg-038934792fd6e7c3f"]
+  vpc_security_group_ids = ["sg-038934792fd6e7c3f"]         #replace the security groups of your zomato instance
   connection {
      type = "ssh"
      user = "ec2-user"
-     private_key = file("./bookmyshow.pem")
+     private_key = file("./bookmyshow.pem")                #replace with the your key pair
      host = self.public_ip
      }
   provisioner "remote-exec" {
