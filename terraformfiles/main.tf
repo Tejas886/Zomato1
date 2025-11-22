02af72b2125ae24004febe03c00db0ae1b71da9d
Tejas886 <116872965+Tejas886@users.noreply.github.com>
2025-11-22 15:04:24 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test-server" {
   ami = "ami-0fa3fe0fa7920f68e"                            #replace with your AMI ID of amazon linux 
   instance_type = "t3.small"
   key_name = "bookmyshow"
-  vpc_security_group_ids = ["sg-038934792fd6e7c3f"]         #replace the security groups of your zomato instance
+  vpc_security_group_ids = ["sg-018becd0736e04e44"]         #replace the security groups of your zomato instance
   connection {
      type = "ssh"
      user = "ec2-user"
